076b78c0ae72276312129c938514c212ccd3c4b4
Kimia Sajjadi <67805546+kimiasj@users.noreply.github.com>
2021-03-02 15:06:33 -0330

    Week 2

--- js/index.js
+++ js/index.js
@@ -1,52 +1,34 @@
-let x = 1;
-let y = "Sahand";
-let z = "Kim";
+let userid = 123;
+let message = `hello user id ${userid}`;
+console.log(message);
 
-x++;
-console.log(x, y);
+const pageLoad = () => {
+    console.log('page completely loaded')
 
-
-// Function
-function myFunction(x, y) {
-    console.log(x, y, z);
-}
-myFunction(1, 2);
-
-
-// Array
-let list = [1, 2, 3];
-list.push(4)
-console.log(list);
-
-// IF
-let answer = true;
-if (answer && 1 == 1 || 2 == 2) {
-    console.log("yes")
-} else {
-    console.log("no")
-}
-
-// FOR
-for(let i = 0; i < list.length; i++) {
-    console.log(list[i])
-}
-
-
-//object
-let user =  {
-    id: 1,
-    name: "Kim"
-}
-
-// functional programming example
-function getUser(id, name) {
-    return {
-        id: id,
-        name: name
+    const myFunction = () => {
+        const request = new XMLHttpRequest();
+        request.addEventListener('load', onLoad)
+        request.open('GET', 'http://localhost:8081/users'); // TODO: enable global cors in server
+        request.send();
+        function onLoad() {
+            const res = JSON.parse(request.response);
+            console.log(res);
+            let dropdown = document.getElementById("usernameInput")
+            for (let i = 0; i < dropdown.length; i++) {
+                dropdown.innerHTML += "<option value=\"" + res[i].id + "\">" + res[i].name + "</option>"
+            
+            }
+            
+            // TODO: show users in dropdown, put user Ids in "value"
+            // TODO: add an option for "add user"
+        }
     }
+    myFunction();
+    
+    
+
+    // TODO: console.log when the user selects "add user" from the dropdown
+    // TODO: when a user is selected and show costs is clicked, make an api call to get the costs and console.log the result
+
 }
-
-let user2 = getUser(2, "Sahand");
-console.log(user2);
-
-let date = new Date();
+window.addEventListener('load', pageLoad);
