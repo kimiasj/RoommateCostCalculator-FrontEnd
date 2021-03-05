87e633c08463b0cbcf4c85ba16526693206b94ee
Kimia Sajjadi <67805546+kimiasj@users.noreply.github.com>
2021-03-05 15:45:09 -0330

    showcostButton

--- js/index.js
+++ js/index.js
@@ -4,28 +4,46 @@ console.log(message);
 
 const pageLoad = () => {
     console.log('page completely loaded')
+    let dropdown = document.getElementById("usernameInput")
 
-    const myFunction = () => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', onLoad)
+    request.open('GET', 'http://localhost:8081/users'); // TODO: enable global cors in server
+    request.send();
+    function onLoad() {
+        const res = JSON.parse(request.response);
+        console.log(res);
+
+        for (let i = 0; i < res.length; i++) {
+            dropdown.innerHTML += "<option value=\"" + res[i].id + "\">" + res[i].name + "</option>"
+
+        }
+
+    }
+
+    dropdown.addEventListener('change', function (event) {
+        if(dropdown.value == 'adduser') {
+            console.log('adding user');
+        }
+    });
+
+
+    let click = document.getElementById("button1");
+    click.addEventListener('click', myFunction )
+    function myFunction (){
+
         const request = new XMLHttpRequest();
-        request.addEventListener('load', onLoad)
-        request.open('GET', 'http://localhost:8081/users'); // TODO: enable global cors in server
+        request.addEventListener('load' , onload)
+        request.open('GET', 'http://localhost:8081/costs?userId=' + dropdown.value); // TODO: enable global cors in server
         request.send();
-        function onLoad() {
+        function onload (){
             const res = JSON.parse(request.response);
             console.log(res);
-            let dropdown = document.getElementById("usernameInput")
-            for (let i = 0; i < dropdown.length; i++) {
-                dropdown.innerHTML += "<option value=\"" + res[i].id + "\">" + res[i].name + "</option>"
-            
-            }
-            
-            // TODO: show users in dropdown, put user Ids in "value"
-            // TODO: add an option for "add user"
-        }
+        }   
+        
+        
     }
-    myFunction();
-    
-    
+   
 
     // TODO: console.log when the user selects "add user" from the dropdown
     // TODO: when a user is selected and show costs is clicked, make an api call to get the costs and console.log the result
